15ffc46fdd4536b98d8ce3f0f2a615be8d0353bc
Serhat <Serhat@DESKTOP-NPCNB1H>
2022-05-30 15:27:39 +0300

    Proje dosyası ekle.

---
+++ VizeOdevi/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.baslatButton = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.puan = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.spaceship = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        CType(Me.spaceship, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.baslatButton)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.puan)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(500, 60)
+        Me.Panel1.TabIndex = 0
+        '
+        'baslatButton
+        '
+        Me.baslatButton.Location = New System.Drawing.Point(157, 20)
+        Me.baslatButton.Name = "baslatButton"
+        Me.baslatButton.Size = New System.Drawing.Size(75, 23)
+        Me.baslatButton.TabIndex = 3
+        Me.baslatButton.Text = "Başla"
+        Me.baslatButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "DÜŞMANDAN KAÇ"
+        '
+        'puan
+        '
+        Me.puan.AutoSize = True
+        Me.puan.Location = New System.Drawing.Point(430, 20)
+        Me.puan.Name = "puan"
+        Me.puan.Size = New System.Drawing.Size(39, 13)
+        Me.puan.TabIndex = 1
+        Me.puan.Text = "Label2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(386, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Puan :"
+        '
+        'spaceship
+        '
+        Me.spaceship.Image = Global.VizeOdevi.My.Resources.Resources.space
+        Me.spaceship.Location = New System.Drawing.Point(205, 368)
+        Me.spaceship.Name = "spaceship"
+        Me.spaceship.Size = New System.Drawing.Size(66, 74)
+        Me.spaceship.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.spaceship.TabIndex = 0
+        Me.spaceship.TabStop = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Black
+        Me.Panel2.Controls.Add(Me.spaceship)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel2.Location = New System.Drawing.Point(0, 60)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(500, 500)
+        Me.Panel2.TabIndex = 1
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 101
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(500, 560)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.spaceship, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents baslatButton As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents puan As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents spaceship As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ VizeOdevi/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Dim sayac As Integer
+    Dim enemy As PictureBox
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Size = New Drawing.Size(516, 599)
+        Me.KeyPreview = True
+        spaceship.Location = New Point(215, spaceship.Location.Y)
+        sayac = 0
+        puan.Text = sayac.ToString
+    End Sub
+    Public Sub enemyOlustur()
+        Dim RastgeleSayi As New Random
+        Dim OlusturulanSayi As Integer = RastgeleSayi.Next(-20, 450)
+
+        Timer1.Enabled = True
+        enemy = New PictureBox
+        enemy.Image = Image.FromFile("C:\Users\serhat\source\repos\VizeOdevi\VizeOdevi\enemy.png")
+        enemy.Visible = True
+        enemy.SizeMode = PictureBoxSizeMode.Zoom
+        enemy.Top = 0
+        enemy.Left = OlusturulanSayi
+        Panel2.Controls.Add(enemy)
+    End Sub
+    Private Sub baslatButton_Click(sender As Object, e As EventArgs) Handles baslatButton.Click
+        Panel2.Controls.Remove(enemy)
+        Timer1.Interval = 101
+        spaceship.Location = New Point(215, spaceship.Location.Y)
+        sayac = 0
+        puan.Text = sayac.ToString
+        enemyOlustur()
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Right Or e.KeyCode = Keys.A Then
+            If spaceship.Location.X <= 0 Then
+            Else
+                spaceship.Left -= 15
+            End If
+        End If
+        If e.KeyCode = Keys.Left Or e.KeyCode = Keys.D Then
+            If spaceship.Location.X >= 430 Then
+            Else
+                spaceship.Left += 15
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        enemy.Top += 10
+        If enemy.Location.Y >= 323 And enemy.Location.Y <= 450 Then
+            If enemy.Location.X >= spaceship.Location.X - 50 And enemy.Location.X <= spaceship.Location.X + 50 Then
+                Timer1.Enabled = False
+                MessageBox.Show("Puanın: " + sayac.ToString, "Oyun Bitti !")
+            End If
+        End If
+        If enemy.Location.Y >= 500 Then
+            sayac += 1
+            puan.Text = sayac.ToString
+            Panel2.Controls.Remove(enemy)
+            If Timer1.Interval = 1 Then
+                Timer1.Interval = 1
+            Else
+                Timer1.Interval -= 10
+            End If
+            Timer1.Enabled = False
+            enemyOlustur()
+        End If
+    End Sub
+End Class
